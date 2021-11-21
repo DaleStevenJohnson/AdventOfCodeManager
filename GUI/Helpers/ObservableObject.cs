621e8210c1216298051a32af238de60e68a151b8
dalestevenjohnson <dalestevenjohnson@gmail.com>
2021-11-21 21:58:12 +0000

    Added Helpers

---
+++ GUI/Helpers/ObservableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TeacherPlanner.Helpers
+{
+
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Raises a property changed event for the property named
+        /// Call this in a property setter like so:
+        /// RaiseAndSetIfChanged(ref _privateField, value)
+        /// </summary>
+        protected virtual bool RaiseAndSetIfChanged<TProperty>(
+            ref TProperty field,
+            TProperty value,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<TProperty>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ GUI/Helpers/SimpleCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GUI.Helpers
+{
+    public class SimpleCommand : SimpleCommand<object>
+    {
+        public SimpleCommand(Action<object> action) : base(action)
+        {
+        }
+    }
+
+    public class SimpleCommand<T> : ICommand where T : class
+    {
+        private readonly Action<T> _action;
+
+        public SimpleCommand(Action<T> action)
+        {
+            _action = action;
+            CanExecuteChanged = (sender, args) => { };
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
