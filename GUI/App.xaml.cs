74bc93a13a5ac9e3448e93f480bf157d247374c8
dalestevenjohnson <dalestevenjohnson@gmail.com>
2021-11-21 21:36:45 +0000

    Added Dependency Injection

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Autofac;
 
 namespace GUI
 {
@@ -13,5 +8,12 @@ namespace GUI
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule(new Installer());
+            var container = containerBuilder.Build();
+            Container.SetInstance(container);
+        }
     }
 }
---
+++ GUI/Container.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autofac;
+
+namespace GUI
+{
+    internal static class Container
+    {
+        private static IContainer? _instance;
+        public static IContainer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    throw new NullReferenceException();
+                return _instance;
+            }
+        }
+
+        internal static void SetInstance(IContainer instance)
+        {
+            _instance = instance;
+        }
+    }
+}
