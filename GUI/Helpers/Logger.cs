903030dd03e2d3c3a60e38ef3aa18c9a66c273a4
dalestevenjohnson <dalestevenjohnson@gmail.com>
2021-11-22 20:46:54 +0000

    Added Logger

---
+++ GUI/Helpers/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GUI.Helpers
+{
+    internal class Logger
+    {
+        private string _filename = "log.txt";
+        internal Logger()
+        {
+
+        }
+
+        internal void Log(string message)
+        {
+            if (!File.Exists(_filename))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(_filename))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                File.WriteAllText(_filename, message);
+            }
+        }
+
+
+    }
+}
